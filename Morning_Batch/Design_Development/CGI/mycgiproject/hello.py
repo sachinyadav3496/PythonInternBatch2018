e0229105d83d079051c7d3b658e84166dadb1e9b
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-06-16 11:09:52 +0530

    Cgi files added

---
+++ Morning_Batch/Design_Development/CGI/mycgiproject/hello.py
@@ -0,0 +1,21 @@
+#!C:\\ProgramData\\Anaconda3\\python.exe
+
+print("Content-type:text/html")
+print()
+
+page = """
+<!Doctype html>
+<html>
+<head>
+<title>Hello</title>
+<link rel='stylesheet' type='text/css' href='css/main.css' >
+</head>
+<body>
+
+<h1> Welcome to first CGI-SCRIPT</h1>
+<h1> Hello World In CGI
+</body>
+</html>
+"""
+print(page)
+
