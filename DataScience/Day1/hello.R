e7bea6bfcc06d3e0d566d287dac27dae372612ea
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-07-07 18:26:03 +0530

    hello

---
+++ DataScience/Day1/hello.R
@@ -0,0 +1,4 @@
+x = 5
+y = 10
+print("x+y =")
+print(x+y)
---
+++ DesignDevelopment/Flask_Application/app1.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = "Flask Powered WebSite"
+    return render_template('index.html',data=name)
+@app.route('/hello123/')
+def contact():
+    d = {
+            'name':'Flask',
+            'language':'Python',
+            'use':'Web Desgning',
+            'version':0.12,
+            }
+
+    return render_template('contact.html',data=d)
+@app.route('/hello/')
+@app.route('/hello/<string:name>/')
+def hello(name=None):
+    return "Welcome User with id {} ".format(name)
+
+
+if __name__ == "__main__" :
+    app.run(host='localhost',port=80,debug=True)
