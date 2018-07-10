9ca18e14a733faea8f9affd3f50a76ee69ac07a1
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-07-10 11:16:54 +0530

    Added code for flask

--- DesignDevelopment/Flask_app/myflask.py
+++ DesignDevelopment/Flask_app/myflask.py
@@ -1,12 +1,14 @@
-from flask import Flask,render_template,request,make_response
+from flask import Flask,render_template,request,make_response,session
+from flask import redirect,url_for
 app = Flask(__name__)
 import pymysql as sql
+app.secret_key = 'alksjflkjaroashfohr9382y98refhsy93yrhoihfohfiudshiofew9'
 
 @app.route('/')
 def index():
-    if request.cookies.get('name'):
-            name = request.cookies.get('name')
-            password = request.cookies.get('password')
+    if 'name' in session:
+            name = session.get('name')
+            password = session.get('password')
             return render_template('index.html',login=True)
     #return '<h1 style=\'color:red\'>Bye Bye World</h1>'
     return render_template('index.html')
@@ -18,9 +20,9 @@ def hello_world(name=None):
 @app.route('/login/',methods=['POST',"GET"])
 def login():
 
-    if request.cookies.get('name'):
-            name = request.cookies.get('name')
-            password = request.cookies.get('password')
+    if 'name' in session :
+            name = session.get('name')
+            password = session.get('password')
 
     elif request.method == 'POST' :
         name = request.form.get('name')
@@ -42,10 +44,13 @@ def login():
                     'Last Name' : n[2],
                     'Email' : n[3],
                     }
-                resp = make_response(render_template('login.html',data=data))
-                resp.set_cookie('name',name)
-                resp.set_cookie('password',password)
-                return resp
+                session['name'] = name
+                session['password'] = password
+                return render_template('login.html',data=data)
+                #resp = make_response(render_template('login.html',data=data))
+                #resp.set_cookie('name',name)
+                #resp.set_cookie('password',password)
+                #return resp
             else :
                 error = 'Invalid Password Try Again'
                 return render_template('index.html',error=error)
@@ -58,9 +63,12 @@ def login():
 
 @app.route('/logout/')
 def logout():
-    resp = make_response(render_template('index.html'))
-    resp.delete_cookie('name')
-    resp.delete_cookie('password')
-    return resp
+    del session['name']
+    del session['password']
+    return redirect(url_for('index'))
+    #resp = make_response(render_template('index.html'))
+    #resp.delete_cookie('name')
+    #resp.delete_cookie('password')
+    #return resp
 if __name__ == '__main__':
     app.run(host='localhost',port=80,debug=True)
